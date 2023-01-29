e2a2aaae10af623f95e3621b566157786a2925fa
niki-olga <nol.21d@uni-dubna.ru>
2023-01-29 18:15:55 +0300

    added files

---
+++ Library_Console/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Console;
+
+class Person
+{
+    string path = "/Users/fetaniz/Desktop/Study/C#/Library_Console/Library_Console/Login_List.txt";
+    public int ID;
+    public string FirstName { get; set; }
+    public string SecondtName { get; set; }
+    public string UserLogin { get; set; }
+    public string UserPassword { get; set; }
+
+
+
+    public Person(string firstName, string secondtName, string userLogin, string userPassword)
+    {
+        this.ID = this.get_new_id();
+        FirstName = firstName;
+        SecondtName = secondtName;
+        UserLogin = userLogin;
+        UserPassword = userPassword;
+    }
+
+    public int get_new_id()
+    {
+        int max_id = 0;
+        string[] lines = File.ReadLines(path).ToArray();
+        for (int i = 0; i < lines.Length-1; i++)
+        {
+            if (lines[i][0] > max_id)
+            {
+                max_id = lines[i][0];
+            }
+        }
+        return max_id;
+    }
+
+    public int get_id()
+    {
+        return this.ID;
+    }
+
+    public void set_id(int new_id)
+    {
+        string[] lines = File.ReadLines(path).ToArray();
+        for (int i = 0; i < lines.Length-1; i++)
+        {
+            if (lines[i][0] == this.ID)
+            {
+                throw new Exception("такой id уже занят");
+            }
+        }
+        for (int i = 0; i < lines.Length-1; i++)
+        {
+            if (lines[i][0] == this.ID)
+            {
+                lines[i][0] = new_id;
+
+            }
+        }
+    }
+
+
+
+
+
+    public override string ToString()
+    {
+        return "" + this.ID + "," + FirstName + "," + SecondtName + "," + UserLogin + "," + UserPassword;
+    }
+
+}
+
+
---
+++ Library_Console/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Library_Console;
+
+class Program
+{
+    public static void Main()
+    {
+        //выбор действия
+        Console.WriteLine("For Login enter 'Log'");
+        Console.WriteLine("For register enter 'Reg'");
+        string? input = Console.ReadLine();
+
+        if (input == "Log")
+        {
+            Console.Clear();
+            Login();
+        }
+        else if (input == "Reg")
+        {
+            Console.Clear();
+            Reg();
+        }
+        else
+        {
+            Console.WriteLine("Press any button to try again");
+            Console.ReadKey();
+            Console.Clear();
+            Main();
+        }
+    }
+    public static void Login()
+    {
+        //путь к файлу с логинами
+        string path = "/Users/fetaniz/Desktop/Study/C#/Library_Console/Library_Console/Login_List.txt";
+        Console.Write("Username: ");
+        string? username = Console.ReadLine();
+        Console.Write("Password: ");
+        string? password = Console.ReadLine();
+
+        bool loginSuccessful = false;
+        //считываем путь и проверяем файл построчно
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {//данные разделены , сравниваем те, что с 3 и 4 разделителем (логин, пароль)
+                var creds = line.Split(',');
+                if (username == creds[3] && password == creds[4])
+                {
+                    loginSuccessful = true;
+                    break;
+                }
+            }
+            reader.Close();
+        }
+
+        if (loginSuccessful && username.Contains("admin"))
+        {
+            Console.WriteLine("Admin Login Successful! Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            AdminMenu();
+        }
+        else if (loginSuccessful && username.Contains("user"))
+        {
+            Console.WriteLine("User Login Successful! Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            UserMenu();
+        }
+        else
+        {
+            Console.WriteLine("Username or password is incorrect. Try again");
+            Console.ReadKey();
+            Console.Clear();
+            Login();
+        }
+
+        static void AdminMenu()
+        {
+            Console.WriteLine("Admin Main menu");
+            Console.Read();
+        }
+
+        static void UserMenu()
+        {
+            Console.WriteLine("User Main menu");
+            Console.Read();
+        }
+
+    }
+    public static void Reg()
+    {
+        string path = "/Users/fetaniz/Desktop/Study/C#/Library_Console/Library_Console/Login_List.txt";
+        Console.WriteLine("Enter your First name: ");
+        string? firstname = Console.ReadLine();
+        Console.WriteLine("Enter your Second name: ");
+        string? secondname = Console.ReadLine();
+        Console.WriteLine("Enter your username: ");
+        string? username = Console.ReadLine();
+        Console.WriteLine("Enter your password: ");
+        string? password = Console.ReadLine();
+
+        Person newPerson = new Person(firstname, secondname, username, password);
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            while (!reader.EndOfStream)
+            {
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.WriteLine(newPerson.ToString());
+                }
+                break;
+
+            }
+
+            reader.Close();
+        }
+
+        Console.Read();
+    }
+}
